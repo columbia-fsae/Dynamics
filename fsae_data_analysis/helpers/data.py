93da3aeca57c2e1e6e40a030a73405b4584c4706
eliseyangg <93101453+eliseyangg@users.noreply.github.com>
2024-02-04 17:22:21 -0600

    clean and add coolant pres
    changed savefigs to accommodate generate report script, coolant pressure in case to check for anomalies

--- fsae_data_analysis/helpers/data.py
+++ fsae_data_analysis/helpers/data.py
@@ -2,60 +2,37 @@ import pandas as pd
 import numpy as np
 from pyarrow import csv
 
-
 def process_analysis_data(csv_name):
-    y_cols = [
-        "Time",
-        "G Force Long",
-        "G Force Lat",
-        "Damper Pos FL",
-        "Damper Pos FR",
-        "Damper Pos RL",
-        "Damper Pos RR",
-        "Wheel Speed FL",
-        "Wheel Speed FR",
-        "Wheel Speed RL",
-        "Wheel Speed RR",
-        "Steering Wheel Angle",
-        "Brake Pressure Front",
-        "Brake Pressure Rear",
-        "Coolant Temperature",
-        "Coolant Temperature (2)",
-        "GP Vol Flow 1"
-    ]
-
-    df = csv.read_csv(csv_name, csv.ReadOptions(skip_rows=14)).to_pandas()
-    df = df.loc[:, y_cols]
+    df = csv.read_csv(csv_name, csv.ReadOptions(skip_rows = 14)).to_pandas()
+    df = df.filter(regex='Time|G Force|Damper Pos|Wheel Speed|Steering Wheel Angle|Wheel Center|Brake Pressure|Torque|Battery|Coolant|GP Vol Flow')
     df = df.drop(df.iloc[[0]].index)
-
+    # df = df[df["Time"].astype(float) > 1050]
+    # df = df[df["Time"].astype(float) < 2600]
     df = df.reset_index()
     df = df.astype(np.float64)
 
-    # processing + channel math
+    # processing + channel math :')
     process_2023_data(df)
     return df
 
-
 def process_battery_data(csv_name):
-    df = pd.read_csv(csv_name, skiprows=14, low_memory=False)
+    df = pd.read_csv(csv_name, skiprows=14,low_memory=False)
     df = df.filter(regex='Battery Temp|Battery Voltage|Time')
-
     df = df.drop(df.iloc[[0]].index)
-
+    # df = df[df["Time"].astype(float) > 1050]
+    # df = df[df["Time"].astype(float) < 2600]
     df = df.reset_index()
     df = df.astype(np.float64)
-
     return df
 
-
 def process_2023_data(df):
-    # df["G Force Long"] = -1 * df["G Force Long"]
+    df["G Force Long"] = -1 * df["G Force Long"]
     try:
         df["Ground Speed"] = df["Ground Speed"].astype(float)
     except:
-        df["Ground Speed"] = df[["Wheel Speed FL", "Wheel Speed FR"]].max(axis=1).astype(float)
+        df["Ground Speed"] = df[["Wheel Speed FL","Wheel Speed FR"]].max(axis=1).astype(float)
 
     df["Brake Bias"] = df["Brake Pressure Front"]/(df["Brake Pressure Front"]+df["Brake Pressure Rear"])
-    # df["Corner Radius"] = (df["Ground Speed"].pow(2))/df["G Force Lat"]
+    df["Corner Radius"] = (df["Ground Speed"].pow(2))/df["G Force Lat"]
     df["Longitudinal Slip RR"] = (df["Wheel Speed RR"] - df["Ground Speed"])/df["Ground Speed"]
     df["Longitudinal Slip RL"] = (df["Wheel Speed RL"] - df["Ground Speed"])/df["Ground Speed"]
--- fsae_data_analysis/helpers/graph.py
+++ fsae_data_analysis/helpers/graph.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import linear_model
@@ -8,14 +9,11 @@ cycol = cycle('bgrcmk')
 
 global fig_id
 fig_id = 0
-
-
 # graph damper position:
-def graph_damper_pos(df, saveFig=False, testName=""):
+def graph_damper_pos(df):
     global fig_id
     fig_id = fig_id+1
-    fig = plt.figure(fig_id)
-    ax = plt.gca()
+    fig = plt.figure(fig_id, figsize=(12,6))
 
     damperPosCols = ["Damper Pos FL", "Damper Pos FR", "Damper Pos RL", "Damper Pos RR"]
     for col in damperPosCols:
@@ -33,20 +31,20 @@ def graph_damper_pos(df, saveFig=False, testName=""):
 
     # Put a legend to the right of the current axis
     ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-    if saveFig:
-        plt.tight_layout()
-        plt.savefig("./outputs/%s_damper_pos.png" % df.name, bbox_inches='tight')
+
+    plt.savefig('Damper_Pos.png', bbox_inches='tight')
 
 # graph damper velocity:
-def graph_damper_vel(df, saveFig=False):
+def graph_damper_vel(df):
     global fig_id
     fig_id = fig_id+1
-    fig = plt.figure(fig_id)
-    ax = plt.gca()
+    fig = plt.figure(fig_id, figsize=(12,6))
 
-    damperVelCols = ["Damper Velocity FL (mm)", "Damper Velocity FR (mm)", "Damper Velocity RL (mm)", "Damper Velocity RR (mm)"]
+    # damperVelCols = ["Damper Velocity FL (mm)", "Damper Velocity FR (mm)", "Damper Velocity RL (mm)", "Damper Velocity RR (mm)"]
+    damperVelCols = ["Damper Velocity FL", "Damper Velocity FR", "Damper Velocity RL", "Damper Velocity RR"]
     for col in damperVelCols:
         y = df[col].astype(float)
+        ax = plt.gca()
         ax.plot(df["Time"].astype(float), y, markersize=5, label = col, color=next(cycol))
     
     plt.xlabel("Time (s)")
@@ -59,16 +57,13 @@ def graph_damper_vel(df, saveFig=False):
 
     # Put a legend to the right of the current axis
     ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-    if saveFig:
-        plt.tight_layout()
-        plt.savefig("./outputs/%s_damper_vel.png" % df.name, bbox_inches='tight')
-
+    plt.savefig('Damper_Vel.png', bbox_inches='tight')
 
 # graph steering wheel angle
-def graph_steer(df, saveFig=False):
+def graph_steer(df):
     global fig_id
     fig_id = fig_id+1
-    fig = plt.figure(fig_id)
+    fig = plt.figure(fig_id, figsize=(12,6))
 
     y = df["Steering Wheel Angle"].astype(float)
     ax = plt.gca()
@@ -76,16 +71,15 @@ def graph_steer(df, saveFig=False):
     plt.xlabel("Time (s)")
     plt.ylabel("Steering Wheel Angle (def)")
     plt.title("Steering Wheel Angle v.s. Time")
-    if saveFig:
-        plt.tight_layout()
-        plt.savefig("./outputs/%s_steer_angle.png" % df.name, bbox_inches='tight')
+    
+    plt.savefig('Steering_Wheel_Angle.png', bbox_inches='tight')
 
 
 # graph the g-g diagram
-def graph_gg(df, saveFig=False):
+def graph_gg(df):
     global fig_id
     fig_id = fig_id+1
-    fig = plt.figure(fig_id)
+    fig = plt.figure(fig_id, figsize=(8,6))
 
     # make sure that these are the right ones, may need to flip signs/axes etc.
     ayCol = "G Force Lat"
@@ -96,20 +90,19 @@ def graph_gg(df, saveFig=False):
     plt.xlabel("Ay (g)")
     plt.ylabel("Ax (g)")
     plt.title("g-g Diagram")
-    if saveFig:
-        plt.tight_layout()
-        plt.savefig("./outputs/%s_gg.png" % df.name, bbox_inches='tight')
+
+    plt.savefig('GG.png', bbox_inches='tight')
 
 # graph ax v.s. time
-def graph_a(df, saveFig=False):
+def graph_a(df):
     global fig_id
     fig_id = fig_id+1
-    fig = plt.figure(fig_id)
-    ax = plt.gca()
+    fig = plt.figure(fig_id, figsize=(12,6))
 
     aCols = ["G Force Long", "G Force Lat"]
     for col in aCols:
         y = df[col]
+        ax = plt.gca()
         ax.plot(df["Time"], y, markersize=5, label = col, color=next(cycol))
     
     plt.xlabel("Time (s)")
@@ -122,22 +115,27 @@ def graph_a(df, saveFig=False):
 
     # Put a legend to the right of the current axis
     ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-    if saveFig:
-        plt.tight_layout()
-        plt.savefig("./outputs/%s_accel.png" % df.name, bbox_inches='tight')
+
+    plt.savefig('Acc.png', bbox_inches='tight')
 
 # graph wheel speed:
-def graph_wheel_speed(df, saveFig=False):
+def graph_wheel_speed(df):
+    wheelSpeedCols = ["Wheel Speed FL", "Wheel Speed FR", "Wheel Speed RL", "Wheel Speed RR"]
+    try:
+        df[wheelSpeedCols]
+    except:
+        print("Error: Missing wheel speed column(s)")
+
     global fig_id
     fig_id = fig_id+1
-    fig = plt.figure(fig_id)
-    ax = plt.gca()
+    fig = plt.figure(fig_id, figsize=(12,6))
 
-    wheelSpeedCols = ["Wheel Speed FL", "Wheel Speed FR", "Wheel Speed RL", "Wheel Speed RR"]
+    
     for col in wheelSpeedCols:
-        y = df[col].to_numpy()
+        y = df[col]
+        ax = plt.gca()
         ax.plot(df["Time"], y, markersize=5, label = col, color=next(cycol))
-
+    
     plt.xlabel("Time (s)")
     plt.ylabel("Wheel Speed (km/h)")
     plt.title("Wheel Speed" + " v.s. Time")
@@ -148,34 +146,34 @@ def graph_wheel_speed(df, saveFig=False):
 
     # Put a legend to the right of the current axis
     ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-    if saveFig:
-        plt.tight_layout()
-        plt.savefig("./outputs/%s_wheel_speed.png" % df.name, bbox_inches='tight')
+
+    plt.savefig('Wheel_Speed.png', bbox_inches='tight')
 
 
-def graph_damper_vel_hist(df, saveFig=False):
+def graph_damper_vel_hist(df):
+    try: 
+        df[["Damper Velocity FL (in)","Damper Velocity FR (in)","Damper Velocity RL (in)","Damper Velocity RR (in)"]]
+    except:
+        print("Error: Missing Damper Velocity Column(s)")
     global fig_id
     fig_id = fig_id+1
-    fig = plt.figure(fig_id)
+    fig = plt.figure(fig_id, figsize=(12,10))
 
     fig, ((ax1, ax2),(ax3, ax4)) = plt.subplots(2, 2, num=fig_id)
     graph_histogram(df, "Damper Velocity FL (in)",axes = ax1)
     graph_histogram(df, "Damper Velocity FR (in)",axes = ax2)
     graph_histogram(df, "Damper Velocity RL (in)",axes = ax3)
     graph_histogram(df, "Damper Velocity RR (in)",axes = ax4)
-    if saveFig:
-        plt.tight_layout()
-        plt.savefig("./outputs/%s_damper_vel.png" % df.name, bbox_inches='tight')
 
+    plt.savefig('Damper_Vel_Hist.png', bbox_inches='tight')
 
-def graph_histogram(df, col, axes, saveFig=False):
+def graph_histogram(df, col, axes):
     bins = []
     for i in range(-20, 21):
         bins.append(i)
     x = df.hist(column=col, bins=bins, grid=False, ax = axes, color='#86bf91', zorder=2, rwidth=0.9)
-        # Despine
-
-    # print(x)
+    
+    # Despine
     x = x[0]
     x.spines['right'].set_visible(False)
     x.spines['top'].set_visible(False)
@@ -200,8 +198,12 @@ def graph_histogram(df, col, axes, saveFig=False):
     # Format y-axis label
     # x.yaxis.set_major_formatter(StrMethodFormatter('{x:,g}'))
 
+def graph_left_turn_roll(df):
+    try:
+        df[["G Force Lat (downsampled)", "G Force Lat (downsampled)", "Front Roll Angle (downsampled)","Rear Roll Angle (downsampled)","Total Roll Angle (downsampled)"]]
+    except:
+        print("Error: No roll data (right turn)")
 
-def graph_left_turn_roll(df, date, saveFig=False):
     global fig_id
     fig_id = fig_id+1
     fig = plt.figure(fig_id)
@@ -231,7 +233,7 @@ def graph_left_turn_roll(df, date, saveFig=False):
 
     plt.xlabel("Lateral accel")
     plt.ylabel("Roll Angle (downsampled) (deg)")
-    plt.title("Roll Angle v.s. Lateral Accel (left turn) for " + date)
+    plt.title("Roll Angle v.s. Lateral Accel (left turn)")
 
     # Shrink current axis by 20%
     box = ax.get_position()
@@ -239,10 +241,17 @@ def graph_left_turn_roll(df, date, saveFig=False):
 
     # Put a legend to the right of the current axis
     ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+
+    plt.savefig('Left_Turn_Roll.png', bbox_inches='tight')
     return slope_front, slope_rear, slope_total
 
 
-def graph_right_turn_roll(df, date, saveFig=False):
+def graph_right_turn_roll(df):
+    try:
+        df[["G Force Lat (downsampled)", "G Force Lat (downsampled)", "Front Roll Angle (downsampled)","Rear Roll Angle (downsampled)","Total Roll Angle (downsampled)"]]
+    except:
+        print("Error: No roll data (right turn)")
+
     global fig_id
     fig_id = fig_id+1
     fig = plt.figure(fig_id)
@@ -266,13 +275,14 @@ def graph_right_turn_roll(df, date, saveFig=False):
     ax.scatter(accel_right, right_roll_rear, marker = "o", s = 1, color=next(cycol),label = "rear roll angle = " + str(slope_rear) + "x + " + str(intercept_rear))
     ax.scatter(accel_right, right_roll_total, marker = "o", s = 1, color=next(cycol),label = "total roll angle = " + str(slope_total) + "x + " + str(intercept_total))
 
+
     abline(slope_front, intercept_front, "front regression")
     abline(slope_rear, intercept_rear, "rear regression")
     abline(slope_total, intercept_total, "total regression")
 
     plt.xlabel("Lateral accel")
     plt.ylabel("Roll Angle (downsampled) (deg)")
-    plt.title("Roll Angle v.s. Lateral Accel (right turn) for " + date)
+    plt.title("Roll Angle v.s. Lateral Accel (right turn)")
 
     # Shrink current axis by 20%
     box = ax.get_position()
@@ -281,26 +291,30 @@ def graph_right_turn_roll(df, date, saveFig=False):
     # Put a legend to the right of the current axis
     ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
+    plt.savefig('Right_Turn_Roll.png', bbox_inches='tight')
     return slope_front, slope_rear, slope_total
 
 
 def linear_regression(accel, roll):
-    # slope, intercept = np.polyfit(accel, roll, 1)
+    #slope, intercept = np.polyfit(accel, roll, 1)
     lr = linear_model.LinearRegression()
     lr.fit(accel.values.reshape(len(accel), 1), roll)
 
     return lr.coef_,  lr.intercept_
 
-
-def graph_rollvtime(df, saveFig=False):
+def graph_rollvtime(df):
+    rollAngleCols = ["Front Roll Angle", "Rear Roll Angle", "G Force Lat"] #"Total Roll Angle"
+    try:
+        df[rollAngleCols]
+    except:
+        print("Error: No roll angle column(s)")
     global fig_id
     fig_id = fig_id+1
-    fig = plt.figure(fig_id)
-    ax = plt.gca()
+    fig = plt.figure(fig_id, figsize=(12,6))
 
-    rollAngleCols = ["Front Roll Angle", "Rear Roll Angle", "G Force Lat"]  # "Total Roll Angle"
     for col in rollAngleCols:
         y = df[col].astype(float)
+        ax = plt.gca()
         ax.plot(df["Time"].astype(float), y, markersize=5, label = col, color=next(cycol))
     
     plt.xlabel("Time (s)")
@@ -313,20 +327,24 @@ def graph_rollvtime(df, saveFig=False):
 
     # Put a legend to the right of the current axis
     ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-    if saveFig:
-        plt.tight_layout()
-        plt.savefig("./outputs/%s_roll_angle_accel.png" % df.name, bbox_inches='tight')
 
-def graph_long_slip(df, saveFig=False):
+    plt.savefig('Roll_Time.png', bbox_inches='tight')
+
+def graph_long_slip(df):
+    longSlipPercent = ["Longitudinal Slip RL", "Longitudinal Slip RR"]
+    try: 
+        df[longSlipPercent]
+    except:
+        print("Error: No longitudinal slip percent data")
+
     global fig_id
     fig_id = fig_id+1
-    fig = plt.figure(fig_id)
-    ax = plt.gca()
+    fig = plt.figure(fig_id, figsize=(12,6))
 
-    longSlipPercent = ["Longitudinal Slip RL", "Longitudinal Slip RR"]
     for col in longSlipPercent:
-        y = df[col].astype(float)
-        ax.plot(df["Time"].astype(float), y, markersize=5, label = col, color=next(cycol))
+        y = df[col]
+        ax = plt.gca()
+        ax.plot(df["Time"], y, markersize=5, label = col, color=next(cycol))
     
     plt.xlabel("Time (s)")
     plt.ylabel("Longitudinal Slip Percent (%)")
@@ -338,21 +356,23 @@ def graph_long_slip(df, saveFig=False):
 
     # Put a legend to the right of the current axis
     ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-    if saveFig:
-        plt.tight_layout()
-        plt.savefig("./outputs/%s_long_slip.png" % df.name, bbox_inches='tight')
 
+    plt.savefig('Long_Slip.png', bbox_inches='tight')
 
 # brake pressures v.s. time
-def graph_brake_pres(df, saveFig=False):
+def graph_brake_pres(df):
+    brakePressures = ["Brake Pressure Front", "Brake Pressure Rear"]
+    try: 
+        df[brakePressures]
+    except:
+        print("Error: No brake pressure data")
     global fig_id
     fig_id = fig_id+1
-    fig = plt.figure(fig_id)
-    ax = plt.gca()
+    fig = plt.figure(fig_id, figsize=(12,6))
 
-    brakePressures = ["Brake Pressure Front", "Brake Pressure Rear"]
     for col in brakePressures:
         y = df[col].astype(float)
+        ax = plt.gca()
         ax.plot(df["Time"].astype(float), y, markersize=5, label = col, color=next(cycol))
     
     plt.xlabel("Time (s)")
@@ -365,75 +385,39 @@ def graph_brake_pres(df, saveFig=False):
 
     # Put a legend to the right of the current axis
     ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-# brake bias v.s. front pressure (scatter plot)
-    if saveFig:
-        plt.tight_layout()
-        plt.savefig("./outputs/%s_brake_pres.png" % df.name, bbox_inches='tight')
+    plt.savefig('Brake_Pres.png', bbox_inches='tight')
 
 
-# battery temperatures
-def graph_battery_temps(df, saveFig=False):
-    global fig_id
-    fig_id = fig_id+1
-    fig = plt.figure(fig_id)
-    ax = plt.gca()
-
-    for col in df.filter(like="Battery Temp").columns:
-        ax.plot(df["Time"].astype(float), df[col], markersize=5, label = col, color=next(cycol))
-    
-    plt.xlabel("Time (s)")
-    plt.ylabel("Battery Temps (C)")
-    plt.title("Battery Temps" + " v.s. Time")
-
-    # Shrink current axis by 20%
-    box = ax.get_position()
-    ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
-
-    # Put a legend to the right of the current axis
-    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-
-    if saveFig:
-        plt.tight_layout()
-        plt.savefig("./outputs/%s_battery_temp.png" % df.name, bbox_inches='tight')
-
-
-# battery voltages
-def graph_battery_volts(df, saveFig=False):
-    global fig_id
-    fig_id = fig_id+1
-    fig = plt.figure(fig_id)
-    ax = plt.gca()
-
-    for col in df.filter(like="Battery Voltage").columns:
-        ax.plot(df["Time"].astype(float), df[col], markersize=5, label = col, color=next(cycol))
-    
-    plt.xlabel("Time (s)")
-    plt.ylabel("Battery Voltage (V)")
-    plt.title("Battery Voltages" + " v.s. Time")
-
-    # Shrink current axis by 20%
-    box = ax.get_position()
-    ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
-
-    # Put a legend to the right of the current axis
-    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-    if saveFig:
-        plt.tight_layout()
-        plt.savefig("./outputs/%s_battery_volts.png" % df.name, bbox_inches='tight')
-
-
-def abline(slope, intercept, label):
-    """Plot a line from slope and intercept"""
-    axes = plt.gca()
-    x_vals = np.array(axes.get_xlim())
-    y_vals = intercept + slope * x_vals
-    plt.plot(x_vals, y_vals, '--', color=next(cycol),label=label)
-
-
-def graph_coolant_temp(df, saveFig=False):
+# torque
+def graph_torque_feedback(df):
     global fig_id
     fig_id = fig_id + 1
-    fig = plt.figure(fig_id)
+    fig = plt.figure(fig_id, figsize=(12,6))
+
+    plt.xlabel("Time (s)")
+    plt.ylabel("Torque Feedback (N.cm)")
+    plt.title("Torque Feedback" + " v.s. Time")
+    plt.plot(df['Time'], df['Torque Feedback'])
+    plt.savefig('Torque_Feedback.png', bbox_inches='tight')
+
+def graph_engine_torque(df):
+    global fig_id
+    fig_id = fig_id + 1
+    fig = plt.figure(fig_id, figsize=(12,6))
+
+    plt.xlabel("Time (s)")
+    plt.ylabel("Engine Torque (N.m)")
+    plt.title("Engine Torque" + " v.s. Time")
+    plt.plot(df['Time'], df['Engine Torque'])
+    plt.savefig('Engine_Torque.png', bbox_inches='tight')
+
+
+#coolant
+def graph_coolant_temp(df):
+    global fig_id
+    fig_id = fig_id + 1
+    fig = plt.figure(fig_id, figsize=(12,6))
+
     ax = plt.gca()
 
     for col in df.filter(like="Coolant Temperature").columns:
@@ -450,25 +434,84 @@ def graph_coolant_temp(df, saveFig=False):
     # Put a legend to the right of the current axis
     ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
-    if saveFig:
-        plt.tight_layout()
-        plt.savefig("./outputs/%s_coolant_temp.png" % df.name, bbox_inches='tight')
+    plt.savefig('Coolant_Temp.png', bbox_inches='tight')
 
-
-def graph_coolant_flow(df, saveFig=False):
+def graph_coolant_pres(df):
     global fig_id
     fig_id = fig_id + 1
-    fig = plt.figure(fig_id)
+    fig = plt.figure(fig_id, figsize=(12,6))
 
-    y = df['GP Vol Flow 1'].to_numpy()
-    ax = plt.gca()
-    ax.plot(df["Time"].astype(float), y, markersize=5)
+    plt.xlabel("Time (s)")
+    plt.ylabel("Coolant Pressure (kPa)")
+    plt.title("Coolant Pressure v.s. Time")
+    plt.plot(df['Time'], df['Coolant Pressure'])
+    plt.savefig('Coolant_Pres.png', bbox_inches='tight')
+
+def graph_coolant_flow(df):
+    global fig_id
+    fig_id = fig_id + 1
+    fig = plt.figure(fig_id, figsize=(12,6))
 
     plt.xlabel("Time (s)")
     plt.ylabel("Coolant Flow (ml/s)")
-    plt.title("Coolant Flow" + " v.s. Time")
+    plt.title("Coolant Flow v.s. Time")
+    plt.plot(df['Time'], df['GP Vol Flow 1'])
+    plt.savefig('Coolant_Flow.png', bbox_inches='tight')
 
-    if saveFig:
-        plt.tight_layout()
-        plt.savefig("./outputs/%s_coolant_flow.png" % df.name, bbox_inches='tight')
+# brake bias v.s. front pressure (scatter plot)
 
+
+# battery temperatures
+def graph_battery_temps(df):
+    global fig_id
+    fig_id = fig_id+1
+    fig = plt.figure(fig_id, figsize = (12,6))
+
+    for col in df.filter(like = "Battery Temp").columns:
+        ax = plt.gca()
+        ax.plot(df["Time"].astype(float), df[col], markersize=5, label = col, color=next(cycol))
+    
+    plt.xlabel("Time (s)")
+    plt.ylabel("Battery Temps (C)")
+    plt.title("Battery Temps" + " v.s. Time")
+
+    # Shrink current axis by 20%
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+
+    # Put a legend to the right of the current axis
+    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5), fontsize=7, ncol=2)
+
+    plt.savefig('Battery_Temps.png', bbox_inches='tight')
+
+
+# battery voltages
+def graph_battery_volts(df):
+    global fig_id
+    fig_id = fig_id+1
+    fig = plt.figure(fig_id, figsize = (12,6))
+
+    for col in df.filter(like = "Battery Voltage").columns:
+        ax = plt.gca()
+        ax.plot(df["Time"].astype(float), df[col], markersize=5, label = col, color=next(cycol))
+    
+    plt.xlabel("Time (s)")
+    plt.ylabel("Battery Voltage (V)")
+    plt.title("Battery Voltages" + " v.s. Time")
+
+    # Shrink current axis by 20%
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+
+    # Put a legend to the right of the current axis
+    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5), fontsize=7, ncol=2)
+
+    plt.savefig('Battery_Volts.png', bbox_inches='tight')
+
+
+def abline(slope, intercept, label):
+    """Plot a line from slope and intercept"""
+    axes = plt.gca()
+    x_vals = np.array(axes.get_xlim())
+    y_vals = intercept + slope * x_vals
+    plt.plot(x_vals, y_vals, '--', color=next(cycol),label=label)
